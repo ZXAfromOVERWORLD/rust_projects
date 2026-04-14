e07ab2721147f1256ff80d1cad23ce3867bb2ea1
Siddham Negi <siddhamnegi@gmail.com>
2026-04-14 13:20:48 +0530

    Day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Enter the number of digits");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error has occured");
+    let input : i128 = input.trim().parse().unwrap();
+    let ans = generate_fib(input);
+    println!("{ans:?}");
+}
+
+pub fn generate_fib(n : i128) -> Vec<i128>{
+    let mut res = Vec::new();
+
+    if n >= 1{
+        res.push(0);
+    }
+
+    if n>= 2{
+        res.push(1);
+    }
+
+    for i in 2..n{
+        let cur = res[i as usize - 1 ] + res[i as usize - 2];
+        res.push(cur);
+    }
+    res
 }

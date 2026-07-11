385dca04cb963a2b1ac13e884ed5c80c6128526c
Siddham Negi <siddhamnegi@gmail.com>
2026-07-11 23:27:13 +0530

    Day17

---
+++ day17/src/main.rs
@@ -0,0 +1,57 @@
+use std::num::ParseFloatError;
+use std::io::{self, Write};
+
+fn main() {
+    println!("Error Handling Calculator");
+    loop{
+        println!("\n1. Add | 2. Divide | 3. Exit");
+        let choice = input("Choose an option");
+
+        match choice.as_str(){
+            "1" => {
+                match parse_two_numbers(){
+                    Ok((a,b)) => println!("Result {}", a+b),
+                    Err(e) => eprintln!("Error {}",e), 
+                }
+            },
+            "2" => match parse_two_numbers(){
+                Ok((a,b)) => match divide(a,b){
+                    Ok(result) => println!("Result {}", result),
+                    
+                    Err(e) => eprintln!("Error {}",e), 
+                },
+
+                Err(e) => eprintln!("Error {}",e), 
+            },
+
+            "3" => {
+                println!("Exiting");
+                break;
+            },
+            _ => {
+                println!("Invalid Choice");
+            },
+        }
+    }
+}
+
+fn input(s : &str) -> String{
+    println!("{s}");
+    let mut t = String::new();
+    io::stdin().read_line(&mut t).unwrap();
+    t.trim().to_string()
+}
+
+fn parse_two_numbers() -> Result<(f64, f64), ParseFloatError>{
+    let a = input("Enter the number a").parse::<f64>()?;
+    let b = input("Enter the number b").parse::<f64>()?;
+    Ok((a,b))
+}
+
+fn divide(a:f64 , b:f64) -> Result<f64, String>{
+    if b == 0.0{
+        Err("Cannot Divide With 0".to_string())
+    } else{
+        Ok(a/b)
+    }
+}
